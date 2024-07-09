012df529b9240428a5ad78de9c6598aa0cf08577
Gifty <giftyaningg@gmail.com>
2024-07-09 19:22:44 +0100

    Pop function and styling

---
+++ index.js
@@ -0,0 +1,23 @@
+const popUp = document.getElementById("popUp");
+const icon = document.querySelector(".icon");
+
+icon.addEventListener("mouseenter", function () {
+  popUp.style.display = "flex";
+
+});
+
+icon.addEventListener("mouseleave", function () {
+  setTimeout(() => {
+    if (!popUp.matches(":hover")) {
+      popUp.style.display = "none";
+    }
+  }, 100);
+});
+
+popUp.addEventListener("mouseenter", function () {
+  popUp.style.display = "flex";
+});
+
+popUp.addEventListener("mouseleave", function () {
+  popUp.style.display = "none";
+});
